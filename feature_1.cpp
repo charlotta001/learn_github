87690e790ecb2c0596f48b861fa3bc6b9c401928
Abdur-Rahman-ctrl <abdurrahman2722007@gmail.com>
2026-04-06 00:48:32 +0700

    Update feature_1.cpp
    menambahkan sapaan ke entry point

--- feature_1.cpp
+++ feature_1.cpp
@@ -1,8 +1,11 @@
 #include"halo.h"
+#include"salam.h"
 
 #include <iostream>
 
 int main(){
+    std::cout<<menyapa();
     std::cout << "hello, world" << std::endl;
     std::cout << "feature_1 update 1" << std::endl;
+    hitung();
 }
